ba59d89da92eacaaa0d305cce7bff9d689f1131a
unknown <kiss841218@gmail.com>
2019-05-20 22:46:14 +0800

    New PUT method

--- RTfirebaseController.py
+++ RTfirebaseController.py
@@ -1,5 +1,5 @@
 from firebase import firebase
-import firebaseData
+import RTfirebaseData
 
 #由輸入資料的NO取得KeyID
 def CheckKey(no, datas):
@@ -12,21 +12,21 @@ def CheckKey(no, datas):
     return key_id
 
 #查詢
-def CBGet():
+def RTDGet():
     return fb.get(table, None)
     '''
     for key,value in result.items():
         print("id={}\tno={}\tname={}".format(key,value["no"],value["name"]))
     '''
 #儲data至firebase
-def CBPost():
+def RTDPost():
     for data in datas:
         fb.post(table, data)
         print("{} 儲存完畢".format(data))
 
 #從firebase中移除指定NO那層所有資料
-def CBDelete():
-    datas = CBGet()
+def RTDDelete():
+    datas = RTDGet()
     no = input("請輸入要刪除的章節NO\n")
     key_id = CheckKey(int(no), datas)
     if(key_id != ''):
@@ -35,15 +35,27 @@ def CBDelete():
     else:
         print("並無此章節")
 
+#修改firebase
+def RTDPut():
+    datas = RTDGet()
+    no = input("請輸入要修改的章節NO\n")
+    name = input("請輸入姓名\n")
+    key_id = CheckKey(int(no), datas)
+    data = {"no":int(no), "name":name}
+    if(key_id != ''):
+        fb.put(table, data=data, name=key_id) #table為路徑，data為要修改的資料，name是KEY
+        print("修改完畢")
+    else:
+        print("並無此章節")
+    
+
     
 ###主程式 初始化區塊 ###
 datas = [{"no":1, "name":"Voy"},
             {"no":2, "name":"fafalina"},
             {"no":3, "name":"Po-Wen"}]
 
-url = firebaseData.url
-table = firebaseData.table
+url = RTfirebaseData.url
+table = RTfirebaseData.table
 fb = firebase.FirebaseApplication(url, None)
-
-
 
